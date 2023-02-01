67e661ee79f21e00007c29050f402b3e39a0a2d2
MrChen-H <3175073980@qq.com>
2023-02-01 13:43:02 +0800

    2023.1.27 :兼具大小写的最好英文字母

--- Good_String/Good_String.cpp
+++ Good_String/Good_String.cpp
@@ -1,3 +1,13 @@
+/*
+给你一个由英文字母组成的字符串 s ，请你找出并返回 s 中的 最好 英文字母。返回的字母必须为大写形式。如果不存在满足条件的字母，则返回一个空字符串。
+
+最好 英文字母的大写和小写形式必须 都 在 s 中出现。
+
+英文字母 b 比另一个英文字母 a 更好 的前提是：英文字母表中，b 在 a 之 后 出现。
+
+链接: https://leetcode.cn/problems/greatest-english-letter-in-upper-and-lower-case/
+*/
+
 #include<iostream>
 #include<algorithm>
 
